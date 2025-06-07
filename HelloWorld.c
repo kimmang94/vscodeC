9d425a9e16b4bcc5341eeaa4b850f2093c987d2e
kimhyeok_mac <kimmang940715@naver.com>
2025-06-07 14:54:08 +0900

    C추가

--- HelloWorld.c
+++ HelloWorld.c
@@ -1,7 +1,42 @@
 #include <stdio.h>
-
+#include <string.h>
 int main()
-{
-    printf("Hello World");
+{ 
+    int a = 0;
+    int b = 0;
+    char c;
+
+    printf("첫 번째 숫자를 입력해주세요");
+    scanf("%d", a);
+
+    printf("연산을 입력해주세요 (+,-,*,/)");
+    scanf("%c", c);
+
+    printf("두 번째 숫자를 입력해주세요");
+    scanf("%d", b);
+
+    int result = 0;
+
+    switch (c)
+    {
+    case '+':
+        return result = a + b;
+        break;
+    case '-':
+        return result = a - b;
+    case '*':
+        return result = a * b;
+        break;
+    case '/':
+        return result = a / b;
+        break;
+    default:
+        break;
+    }
+
+    printf("해당 결과 값은 : %d", result);
+
+    
+
     return 0;
 }
